568841a645903ea5df2a75e960cb599bd153630a
Zhe Wang <auzwang@gmail.com>
2020-08-14 12:14:14 +1000

    hook up postman logic

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,74 @@
 import { Command, flags } from '@oclif/command'
+import { Postman } from './postman'
 
 class Swagger2PostmanDoc extends Command {
-  static description = 'describe the command here'
+  static description =
+    'CLI tool to generate Postman Documentation from Swagger and update a Collection.'
+
+  helpText = 'See more help with --help.'
 
   static flags = {
     // add --version flag to show CLI version
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
   }
 
-  static args = [{ name: 'swaggerPath' }, { name: 'postmanApiKey' }]
+  static args = [
+    { name: 'file', required: true, description: 'Path to Swagger file' },
+    { name: 'apiKey', required: true, description: 'Postman API key' },
+    {
+      name: 'workspaceId',
+      required: true,
+      description: 'Postman Workspace Id',
+    },
+    { name: 'apiId', required: true, description: 'Postman API Id' },
+    {
+      name: 'apiVersionId',
+      required: true,
+      description: 'Postman API Version Id',
+    },
+    { name: 'schemaId', required: true, description: 'Postman API Schema Id' },
+    {
+      name: 'collectionId',
+      required: true,
+      description: 'Postman API Documentation Collection Id',
+    },
+  ]
 
   async run() {
-    const { args, flags } = this.parse(Swagger2PostmanDoc)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const { args } = this.parse(Swagger2PostmanDoc)
+    const postman = new Postman(args.apiKey)
+    let swaggerJson
+    const file = `${process.cwd()}/${args.file}`
+    try {
+      swaggerJson = require(file || '')
+    } catch (error) {
+      throw new Error(
+        `Failed to require Swagger file: ${file}. ${this.helpText}`
+      )
     }
+    // Update the Postman API Schema based on swagger.json
+    await postman.updateSchema(
+      swaggerJson,
+      args.apiId,
+      args.apiVersionId,
+      args.schemaId
+    )
+    // Generate a Postman Documentation Collection from the Schema.
+    const collectionUid = await postman.generateCollectionFromSchema(
+      args.workspaceId,
+      args.apiId,
+      args.apiVersionId,
+      args.schemaId
+    )
+    // Retrieve the generated collection's data. We'll be updating our main API's Documentation Collection with it.
+    const { item, variable } = await postman.getCollection(collectionUid)
+    // Delete the generated collection otherwise Postman will complain about the rearrangement.
+    await postman.deleteGeneratedCollection(collectionUid)
+    // Retrieve the main Documentation Collection to retain `collection.info`.
+    const { info } = await postman.getCollection(args.collectionId)
+    // Update the main Documentation Collection with the new Schema.
+    await postman.updateCollection(args.collectionId, { info, item, variable })
   }
 }
 
---
+++ src/postman.ts
@@ -0,0 +1,119 @@
+import axios, { AxiosInstance } from 'axios'
+
+interface Collection {
+  info: any
+  item: any
+  variable: any
+}
+
+export class Postman {
+  axiosInstance: AxiosInstance
+
+  constructor(apiKey: string) {
+    this.axiosInstance = axios.create({
+      baseURL: 'https://api.getpostman.com',
+      headers: {
+        'x-api-key': apiKey,
+      },
+    })
+  }
+
+  async updateSchema(
+    swaggerJson: string,
+    apiId: string,
+    apiVersionId: string,
+    schemaId: string
+  ): Promise<void> {
+    try {
+      const schemaPayload = JSON.stringify(swaggerJson)
+      const response = await this.axiosInstance.put(
+        `/apis/${apiId}/versions/${apiVersionId}/schemas/${schemaId}`,
+        {
+          schema: {
+            language: 'json',
+            schema: schemaPayload,
+            type: 'openapi2',
+          },
+        }
+      )
+      console.debug(`${response.status}: Updated Postman API Schema`)
+    } catch (error) {
+      console.error('Failed to update Postman API Schema')
+      throw error
+    }
+  }
+
+  async generateCollectionFromSchema(
+    workspaceId: string,
+    apiId: string,
+    apiVersionId: string,
+    schemaId: string
+  ): Promise<string> {
+    try {
+      const response = await this.axiosInstance.post(
+        `/apis/${apiId}/versions/${apiVersionId}/schemas/${schemaId}/collections`,
+        {
+          name: `Generated: ${Date.now()}`,
+          relations: [{ type: 'documentation' }],
+        },
+        {
+          params: { workspace: workspaceId },
+        }
+      )
+      const collectionUid = response.data.collection.uid
+      console.debug(
+        `${response.status}: Generated Collection from Postman API Schema`
+      )
+      console.debug('Response data: ', response.data)
+      return collectionUid
+    } catch (error) {
+      console.error('Failed to generate Collection from Postman API Schema')
+      throw error
+    }
+  }
+
+  async getCollection(collectionUid: string): Promise<Collection> {
+    try {
+      const response = await this.axiosInstance.get(
+        `/collections/${collectionUid}`
+      )
+      console.debug(`${response.status}: Retrieved Collection ${collectionUid}`)
+      return {
+        info: response.data.collection.info,
+        item: response.data.collection.item,
+        variable: response.data.collection.variable,
+      }
+    } catch (error) {
+      console.error(`Failed to retrieve Collection ${collectionUid}`)
+      throw error
+    }
+  }
+
+  async deleteGeneratedCollection(collectionUid: string): Promise<void> {
+    try {
+      const response = await this.axiosInstance.delete(
+        `/collections/${collectionUid}`
+      )
+      console.debug(`${response.status}: Deleted Collection ${collectionUid}`)
+    } catch (error) {
+      console.error(`Failed to delete Collection ${collectionUid}`)
+      throw error
+    }
+  }
+
+  async updateCollection(
+    collectionUid: string,
+    collection: Collection
+  ): Promise<void> {
+    try {
+      const response = await this.axiosInstance.put(
+        `/collections/${collectionUid}`,
+        { collection }
+      )
+      console.debug(`${response.status}: Updated Collection ${collectionUid}`)
+    } catch (error) {
+      console.error(`Failed to update Collection ${collectionUid}`)
+      throw error
+    }
+  }
+}
